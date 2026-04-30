953c7182d3dcf0c672b0390a17c827c4da46c147
CheneyChenStar <cheneychenstar@gmail.com>
2026-04-30 16:18:34 +0800

    fix: restore correct test assertion

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,7 +5,7 @@ describe('GET /', () => {
   it('should return Hello CI/CD message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.body).toEqual({ message: 'Hello World' });
+    expect(res.body).toEqual({ message: 'Hello CI/CD' });
   });
 });
 
